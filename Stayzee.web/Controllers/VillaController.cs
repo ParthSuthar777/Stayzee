a1a5ed5413dcfc6ca214a9a2296fb9248e318442
ParthSuthar777 <135435866+ParthSuthar777@users.noreply.github.com>
2024-12-28 18:24:53 +0530

    Villa Module | Create Villa - server side validations

--- Stayzee.web/Controllers/VillaController.cs
+++ Stayzee.web/Controllers/VillaController.cs
@@ -12,22 +12,49 @@ namespace Stayzee.web.Controllers
         {
             _context = context;
         }
-
+        /// <summary>
+        /// Villa List View
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
         {
             var villas = _context.Villas.ToList();
             return View(villas);
         }
+        /// <summary>
+        /// Create Villa Form
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Create()
         {
             return View();
         }
+        /// <summary>
+        /// Create Villa Post Method
+        /// </summary>
+        /// <param name="villa"></param>
+        /// <returns></returns>
         [HttpPost]
-        public IActionResult Create12(Villa villa)
+        public IActionResult Create(Villa villa)
         {
-            _context.Villas.Add(villa);
-            _context.SaveChanges();
-            return RedirectToAction("Index","Villa");
+            #region Validate
+            if (string.IsNullOrWhiteSpace(villa.Name) || string.IsNullOrWhiteSpace(villa.Description))
+            {
+                ModelState.AddModelError("", "Name or Description should not be Empty");
+            }
+            if (ModelState.IsValid && villa.Name.Trim().Equals(villa?.Description.Trim(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                ModelState.AddModelError("", "Name should be diffrent from discripiton");
+            }
+            #endregion
+
+            if (ModelState.IsValid)
+            {
+                _context.Villas.Add(villa);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Villa");
+            }
+            return View(villa);
         }
     }
 }
