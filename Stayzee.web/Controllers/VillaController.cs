b4d385d15a4dca7a90cfb92263d3b564032b3d57
ParthSuthar777 <135435866+ParthSuthar777@users.noreply.github.com>
2024-12-28 17:01:06 +0530

    Villa Module | Create Villa and view table form

---
+++ Stayzee.web/Controllers/VillaController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stayzee.Domain.Entities;
+using Stayzee.Infrastructure.Data;
+
+namespace Stayzee.web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VillaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var villas = _context.Villas.ToList();
+            return View(villas);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create12(Villa villa)
+        {
+            _context.Villas.Add(villa);
+            _context.SaveChanges();
+            return RedirectToAction("Index","Villa");
+        }
+    }
+}
